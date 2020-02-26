757e5acb8220702bdc4839e2cbc3c46fb73f9147
benk10 <ben.kaufman10@gmail.com>
2020-02-26 13:14:34 +0200

    Use browserify

--- bundle.js
+++ bundle.js
@@ -1,3 +1,4 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.module = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let submit = function(event) {
   event.preventDefault();
   let xgenToMove = parseInt(document.getElementById("amount").value);
@@ -24,7 +25,6 @@ async function moveGEN (xgenToMove) {
     window.web3 = new Web3(web3.currentProvider);
   } else {
     alert('Non-Ethereum browser detected. You should consider trying MetaMask.');
-    return
   }
 
   let defaultAddress = (await web3.eth.getAccounts())[0];
@@ -98,3 +98,5 @@ async function moveGEN (xgenToMove) {
   tx = await GENTokenContract.methods.transfer(bridgeAddress, weiValue, callData).send()
 
 }
+},{}]},{},[1])(1)
+});
